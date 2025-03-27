e09e7336c7967139cdbeeed69624dee8c70810a6
Trace Herrell <trace.herrell@gmail.com>
2025-03-27 14:35:14 -0700

    Refactor Profile and Layout components for improved structure and navigation

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -12,8 +12,9 @@ const ProfileCard = ({ title, description, link, src }) => (
     <div className="padding">
       <h5>{title}</h5>
       <p>{description}</p>
+
       <nav>
-        <Link to={link} className="primary button small-round">
+        <Link to={link} className="primary  button small-round">
           View Project
         </Link>
       </nav>
@@ -23,7 +24,7 @@ const ProfileCard = ({ title, description, link, src }) => (
 
 function Profile() {
   return (
-    <div className="margin-medium main center">
+    <div className="container">
       <div className="grid">
         <div className="s12 m6">
           <div className="max" />
@@ -59,96 +60,97 @@ function Profile() {
       <div className="large-space" />
 
       <div className="medium-margin large-text">
-        <h2>Experience</h2>
-      </div>
-      <div className="small-space" />
-      <div className="grid medium-margin">
-        <div className="s12 m6 l4">
-          <h5>
-            <i className="fab fa-redhat" /> Red Hat
-          </h5>
-          <p>
-            Red Hat OpenShift on AWS (ROSA), Microsoft Azure Red Hat OpenShift
-            (ARO), Redhat OpenShift AI, Kubernetes, Podman, Argo, ArgoCD, Helm
-            Charts, Operators, Istio, Terraform, Vault.
-          </p>
-        </div>
-        <div className="s12 m6 l4">
-          <h5>
-            <i className="fab fa-microsoft" /> Azure
-          </h5>
-          <p>
-            GitHub Actions, Azure Functions, Azure Kubernetes Service (AKS),
-            Azure API Management, Azure SQL, Cosmos DB, Redis Cache, Azure
-            Active Directory (Entra), Azure App Services, Azure Logic Apps.
-          </p>
-        </div>
+        <h2 className="medium-margin">Experience</h2>
 
-        <div className="s12 m6 l4">
-          <h5>
-            <i className="fab fa-aws" /> Amazon
-          </h5>
-          <p>
-            AWS Lambda, API Gateway, DynamoDB, S3, RDS, CloudFormation,
-            CloudWatch, IAM, Cognito, SNS, SQS, Step Functions, Amplify, Code
-            Build.
-          </p>
-        </div>
-        <div className="s12 m6 l4">
-          <h5>
-            <i className="fa fa-lg fa-code" /> Code
-          </h5>
-          <p>
-            JavaScript, TypeScript, Golang, Python, C#, Java, PHP, HTML, CSS,
-            SCSS, Less, PowerShell, Bash. Node, React, Vue, Svelte, Express,
-            NestJS, NextJS, GraphQL, Webpack, Babel, Jest, Veet, Cypress.
-          </p>
-        </div>
-        <div className="s12 m6 l4">
-          <h5 className="sub-heading">
-            <i className="fa fa-lg fa-database" /> Database
-          </h5>
-          <p>
-            SQL Server, MySQL, PostgreSQL, Oracle, MongoDB, Cosmos DB, Redis
-            Cache, DynamoDB, SQLite, Firebase, Firestore.
-          </p>
-        </div>
-
-        <div className="s12 m6 l4">
-          <h5>
-            <i className="fa fa-lg fa-cloud" /> API
-          </h5>
-          <p>
-            REST, GraphQL, gRPC, SOAP, WebSockets, Serverless, Microservices,
-            Nignx, API Gateway, Swagger, OpenAPI, Postman. Go, Node, Expres,
-            NestJS, .NET Core, Spring Boot.
-          </p>
-        </div>
-      </div>
-
-      <div className="small-space" />
-      <div className="medium-margin">
-        <h2 id="featured-projects">Featured Projects</h2>
         <div className="small-space" />
-        <div className="grid">
-          <ProfileCard
-            title="Ford Configuration"
-            description="Advanced vehicle configuration platform for Ford Engineers"
-            link="/ford"
-            src="ford-config.png"
-          />
-          <ProfileCard
-            title="T-Mobile Nessie"
-            description="Change control and governance for T-Mobile Network devices"
-            link="/tmobile"
-            src="t-mobile-nessie.png"
-          />
-          <ProfileCard
-            title="Bluetooth Qualification"
-            description="Device certification platform for Bluetooth SIG Qualification"
-            link="/bluetooth"
-            src="bluetooth-launchstudio.png"
-          />
+        <div className="grid medium-margin">
+          <div className="s12 m6 l4">
+            <h5>
+              <i className="fab fa-redhat" /> Red Hat
+            </h5>
+            <p>
+              Red Hat OpenShift on AWS (ROSA), Microsoft Azure Red Hat OpenShift
+              (ARO), Redhat OpenShift AI, Kubernetes, Podman, Argo, ArgoCD, Helm
+              Charts, Operators, Istio, Terraform, Vault.
+            </p>
+          </div>
+          <div className="s12 m6 l4">
+            <h5>
+              <i className="fab fa-microsoft" /> Azure
+            </h5>
+            <p>
+              GitHub Actions, Azure Functions, Azure Kubernetes Service (AKS),
+              Azure API Management, Azure SQL, Cosmos DB, Redis Cache, Azure
+              Active Directory (Entra), Azure App Services, Azure Logic Apps.
+            </p>
+          </div>
+
+          <div className="s12 m6 l4">
+            <h5>
+              <i className="fab fa-aws" /> Amazon
+            </h5>
+            <p>
+              AWS Lambda, API Gateway, DynamoDB, S3, RDS, CloudFormation,
+              CloudWatch, IAM, Cognito, SNS, SQS, Step Functions, Amplify, Code
+              Build.
+            </p>
+          </div>
+          <div className="s12 m6 l4">
+            <h5>
+              <i className="fa fa-lg fa-code" /> Code
+            </h5>
+            <p>
+              JavaScript, TypeScript, Golang, Python, C#, Java, PHP, HTML, CSS,
+              SCSS, Less, PowerShell, Bash. Node, React, Vue, Svelte, Express,
+              NestJS, NextJS, GraphQL, Webpack, Babel, Jest, Veet, Cypress.
+            </p>
+          </div>
+          <div className="s12 m6 l4">
+            <h5 className="sub-heading">
+              <i className="fa fa-lg fa-database" /> Database
+            </h5>
+            <p>
+              SQL Server, MySQL, PostgreSQL, Oracle, MongoDB, Cosmos DB, Redis
+              Cache, DynamoDB, SQLite, Firebase, Firestore.
+            </p>
+          </div>
+
+          <div className="s12 m6 l4">
+            <h5>
+              <i className="fa fa-lg fa-cloud" /> API
+            </h5>
+            <p>
+              REST, GraphQL, gRPC, SOAP, WebSockets, Serverless, Microservices,
+              Nignx, API Gateway, Swagger, OpenAPI, Postman. Go, Node, Expres,
+              NestJS, .NET Core, Spring Boot.
+            </p>
+          </div>
+        </div>
+
+        <div className="small-space" />
+        <div className="medium-margin">
+          <h2 id="featured-projects">Featured Projects</h2>
+          <div className="small-space" />
+          <div className="grid">
+            <ProfileCard
+              title="Ford Configuration"
+              description="Advanced vehicle configuration platform for Ford Engineers"
+              link="/ford"
+              src="ford-config.png"
+            />
+            <ProfileCard
+              title="T-Mobile Nessie"
+              description="Change control and governance for T-Mobile Network devices"
+              link="/tmobile"
+              src="t-mobile-nessie.png"
+            />
+            <ProfileCard
+              title="Bluetooth Qualification"
+              description="Device certification platform for Bluetooth SIG Qualification"
+              link="/bluetooth"
+              src="bluetooth-launchstudio.png"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,32 +1,40 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const ButtonLink = ({ path, text }) => {
-  const location = useLocation();
-  const isActive =
-    location.pathname === path
-      ? " primary small-round"
-      : " secondary small-round";
+const NavLink = ({ path, text, icon }) => {
   return (
     <Link to={path}>
-      <button style={{ width: "45px" }} className={isActive}>
-        {text}
-      </button>
+      <i>{icon}</i>
+      <span>{text}</span>
+    </Link>
+  );
+};
+
+const BottomNavLink = ({ path, icon }) => {
+  return (
+    <Link to={path}>
+      <i>{icon}</i>
     </Link>
   );
 };
 
 function Layout({ children }) {
   return (
-    <div className="margin-small main center">
-      <nav className="left m l xl">
-        <ButtonLink path="/" text="Home" />
-        <ButtonLink path="/ford" text="Ford" />
-        <ButtonLink path="/tmobile" text="T-Mobile" />
-        <ButtonLink path="/bluetooth" text="Bluetooth" />
-        <ButtonLink path="/2048" text="2048" />
+    <div>
+      <nav className="margin-small left m l xl">
+        <NavLink path="/" text="Home" icon="home" />
+        <NavLink path="/ford" text="Ford" icon="directions_car" />
+        <NavLink path="/tmobile" text="T-Mobile" icon="phone" />
+        <NavLink path="/bluetooth" text="Bluetooth" icon="bluetooth" />
+        <NavLink path="/2048" text="2048" icon="sports_esports" />
       </nav>
-
-      {children}
+      <nav className="bottom s no-padding">
+        <BottomNavLink path="/" icon="home" />
+        <BottomNavLink path="/ford" icon="directions_car" />
+        <BottomNavLink path="/tmobile" icon="phone" />
+        <BottomNavLink path="/bluetooth" icon="bluetooth" />
+        <BottomNavLink path="/2048" icon="sports_esports" />
+      </nav>
+      <div className=" main responsive">{children}</div>
     </div>
   );
 }
